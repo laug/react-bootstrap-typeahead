9a8db8cf1f8a2c263762f4d8dde60c26f8528198
Eric Giovanola <ericgio@gmail.com>
2023-03-29 22:14:09 -0700

    Remove HOCs and move hooks

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export { default as AsyncTypeahead } from './components/AsyncTypeahead';
 export { default as ClearButton } from './components/ClearButton';
 export { default as Highlighter } from './components/Highlighter';
-export { default as Hint, useHint } from './components/Hint';
+export { default as Hint } from './components/Hint';
 export { default as Input } from './components/Input';
 export { default as Loader } from './components/Loader';
 export { default as Menu } from './components/Menu';
@@ -13,7 +13,8 @@ export { default as TypeaheadInputMulti } from './components/TypeaheadInputMulti
 export { default as TypeaheadInputSingle } from './components/TypeaheadInputSingle';
 export { default as TypeaheadMenu } from './components/TypeaheadMenu';
 
-// HOCs + Hooks
-export * from './behaviors/async';
-export * from './behaviors/item';
-export * from './behaviors/token';
+// Hooks
+export { default as useAsync } from './core/useAsync';
+export { default as useHint } from './core/useHint';
+export { default as useItem } from './core/useItem';
+export { default as useToken } from './core/useToken';
